1207edf3a37e138a19bf1dc348e6a6aa565d676d
Santi <santisanudo@gmail.com>
2018-01-13 11:42:56 +0100

    asdasd

---
+++ practicasExamen/Practica8/src/main/java/practica8Disenho/EstrategiaDinamica.java
@@ -0,0 +1,56 @@
+package practica8Disenho;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import practica8Disenho.excepciones.NoSeHaPodidoConectarABBDD;
+
+
+/**
+ * Estrategia para la solicitud y devolucion de conexiones dinamica
+ *
+ */
+public class EstrategiaDinamica extends Estrategia{
+
+	/**
+	 * Constructor de la clase EstrategiaDinamica
+	 * @param numConexiones
+	 * @throws NoSeHaPodidoConectarABBDD 
+	 */
+	public EstrategiaDinamica(int numConexiones) throws NoSeHaPodidoConectarABBDD{
+		super(numConexiones);
+	}
+
+	/**
+	 * Metodo para la solicitud de una de las conexiones del pool
+	 * @return conexion
+	 * @throws NoSeHaPodidoConectarABBDD
+	 */
+	@Override
+	public Connection solicitarConexion() throws NoSeHaPodidoConectarABBDD{
+		Connection connection = null;
+		if(!getConexiones().isEmpty()){
+			connection = getConexiones().poll();
+		} else {
+			try {
+				connection = getConnection();
+			} catch (SQLException e) {
+				throw new NoSeHaPodidoConectarABBDD();
+			}
+		}
+		return connection;
+	}
+
+	/**
+	 * Metodo para la devolucion de una conexion concedida previamente
+	 * @param connection
+	 * @throws NoSeHaPodidoConectarABBDD
+	 */
+	@Override
+	public void devolverConexion(Connection connection) throws NoSeHaPodidoConectarABBDD {
+		if(getConexiones().size() < numConexiones){
+			getConexiones().add(connection);
+		}
+	}
+
+}
